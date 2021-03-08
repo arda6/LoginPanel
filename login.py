74c00430bdae83b4ed6b515b5fd5128deb1cf264
arda6 <74415046+arda6@users.noreply.github.com>
2021-03-08 15:07:18 +0300

    Update login.py

--- login.py
+++ login.py
@@ -1,7 +1,12 @@
 import random
-from colorama import Fore, Back, Style, init
+from colorama import Fore, Style, init
 import questionary
+from plyer import notification
 init(autoreset=True)
+title = "Cloud Software"
+message = "Cloud Software Panel Signed In"
+message2 = "Cloud Software Panel Could Sined In"
+app_icon = (r"logo.ico")
 uname1 = "admin"
 password1 = "root"
 print(Fore.WHITE + """"
@@ -33,7 +38,17 @@ print("Verify Code :" + dogrulayici +"")
 robot = questionary.text("Verify Code :").ask()
 
 if password1 == password and uname1 == uname and robot == dogrulayici :
+    notification.notify(title=title,
+                        message=message,
+                        app_icon=None,
+                        timeout=8,
+                        toast=False)
     print(Fore.BLUE + "Login Successful")
     print(Style.RESET_ALL)
 else:
+    notification.notify(title=title,
+                        message=message2,
+                        app_icon=None,
+                        timeout=8,
+                        toast=False)
     print(Fore.RED + "Login Unsuccessful")
